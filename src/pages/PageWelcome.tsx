aa56623e7c96e8f4d57da4785ef0c55fc195ded7
Edward Tanguay (windows) <edward@tanguay.info>
2024-10-05 16:16:21 +0200

    showing 1

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -14,6 +14,7 @@ export const PageWelcome = () => {
 				{cloudServices.map(cloudService => {
 					return (
 						<div className="bg-slate-500 p-3 mb-2 rounded" key={cloudService.id}>
+							<img src="http://localhost:4501/images/cloudService_1.png"/>
 							<div className="text-base font-semibold">{cloudService.name}</div>
 							<div className="text-sm italic">{cloudService.description}</div>
 						</div>
